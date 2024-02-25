10d2a9bf57bdd474931c41a3557418270a4e2b3a
Fumi <fumi9999@gmail.com>
2024-02-25 23:16:15 +0900

    terao-san original

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 import React from 'react';
+import Login from './components/Token/Login';
+
 
 export default function Home() {
   return (
       <main>
         <div>
-          {/* <Login /> */}
-          Hello React
+          <Login />
+          {/* Hello React */}
         </div>
       </main>
   );
